1898b8b0b1521c6aa107e4cc5d6fd9cda1fe439e
didaryalcin <ddrylcn@gmail.com>
2023-12-30 19:55:50 +0300

    2commint

--- frontend/App.js
+++ frontend/App.js
@@ -8,19 +8,25 @@ import useGeceModu from "./hooks/geceModuAc";
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [geceModu, toggleDarkMode] = useGeceModu();
- 
 
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-mode", geceModu);
+  }, [geceModu]);
+
+
+
   return (
     <div className={geceModu ? "dark-mode App" : "App"}>
-      <Navbar geceModu={geceModu} setGeceModu={setGeceModu} />
+      <Navbar geceModu={geceModu} setGeceModu={toggleDarkMode} />
       <Charts coinData={coinData} />
     </div>
   );
