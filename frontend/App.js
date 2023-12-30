b7fb6f5fa74756c39f2ca7f38c3573b2b1c92cb8
didaryalcin <ddrylcn@gmail.com>
2023-12-30 19:55:24 +0300

    1commint

--- frontend/App.js
+++ frontend/App.js
@@ -3,10 +3,12 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import useGeceModu from "./hooks/geceModuAc";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [geceModu, setGeceModu] = useState(false);
+  const [geceModu, toggleDarkMode] = useGeceModu();
+ 
 
   useEffect(() => {
     axios
